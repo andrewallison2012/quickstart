1fb2eabbae0387b327710b8211a78fbd28d43037
andrewallison2012 <andrewallison2012@gmail.com>
2017-05-07 04:30:51 -0500

    tour of heros done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
