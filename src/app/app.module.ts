427d57e2127006ed7cc8d146a248a4263e34ba01
andrewallison2012 <andrewallison2012@gmail.com>
2017-05-07 11:04:07 -0500

    good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent }    from './hero-search.component';
+import { HeroSearchService }    from './hero-search.service';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
-  providers: [HeroService],
+  imports: [ BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService),],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, HeroSearchComponent],
+  providers: [HeroService, HeroSearchService],
   bootstrap: [AppComponent]
 
 })
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'hero-detail',
-      templateUrl: './hero-detail.component.html',
+    templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
     @Input() hero: Hero;
@@ -25,4 +25,8 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+    save(): void {
+  this.heroService.update(this.hero)
+    .then(() => this.goBack());
+}
 }
